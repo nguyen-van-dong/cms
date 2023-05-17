3c1f1945e8925824fc40acbc583ee384047dfbd8
nguyen-van-dong <dongbuh@gmail.com>
2023-05-17 21:21:44 +0700

    Update url

--- resources/views/v1/admin/post/index.blade.php
+++ resources/views/v1/admin/post/index.blade.php
@@ -217,5 +217,5 @@
 
   </script>
 
-  <script src="{{ asset('vendor/cms/assets/admin/js/post.js') }}"></script>
+  <script src="{{ asset('vendor/cms/asset/admin/js/post.js') }}"></script>
 @endpush
